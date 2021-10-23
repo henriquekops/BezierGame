4fbff17c4381084eadc1f2d85f3bd4e0d7daba3a
henrique.kops <henrique.kops@acad.pucrs.br>
2021-10-23 16:48:39 -0300

    add first version of project structure

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# dependencias externas
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+__author__ = "Henrique Kops & Gabriel Castro"
+
+if __name__ == '__main__':
+    print("Not implemented yet")
