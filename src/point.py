db14c7373dda19d23222e2499f079f2cad1edc65
henrique.kops <henrique.kops@acad.pucrs.br>
2021-10-23 16:54:39 -0300

    add point module

---
+++ src/point.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# external dependencies
+from OpenGL.GL import (
+    glBegin,
+    GL_POINTS,
+    glVertex2f,
+    glEnd
+)
+
+__author__ = "Henrique Kops & Gabriel Castro"
+__credits__ = "Marcio Sarroglia Pinho"
+
+class Point:
+
+    """
+    Point class
+    """
+
+    def __init__(self, x:int=0, y:int=0) -> None:
+        self.x = x
+        self.y = y
+
+    def draw(self) -> None:
+        """
+        Draw this point
+        """
+        glBegin(GL_POINTS)
+        glVertex2f(self.x, self.y)
+        glEnd()
