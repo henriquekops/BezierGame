27c2dff59142f7c2b3203b1c6bfd94d23aa36fa5
henriquekops <henriquekops@gmail.com>
2021-10-26 22:20:12 -0300

    improve curve.py

--- src/objects/curve.py
+++ src/objects/curve.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+# built-in dependencies
+from math import pow
+
+# project dependencies
 from src.basic.point import Point
 
 __author__ = "Henrique Kops & Gabriel Castro"
@@ -11,59 +15,30 @@ class Curve:
     Curve class
     """
 
-    def __init__(self, P0:Point, P1:Point, P2:Point) -> None:
-
-        self.P[0] = P0
-        self.P[1] = P1
-        self.P[2] = P2
+    def __init__(self, p0:Point, p1:Point, p2:Point) -> None:
+        self.p0 = p0
+        self.p1 = p1
+        self.p2 = p2
         self.curve_calcuation()
-        pass
 
     def curve_calcuation(self) -> None:
-
-        dt = 1.0/50 # dt - delta T
+        dt = 1.0/50
         t = dt
-        tc = 0 #tc - comprimento total da curva
-        p1 = self.bezier_calculation(0.0)
+        length = 0
+        p1 = self.bezier(0.0)
         while( t < 1.0):
-            p2 = self.bezier_calculation(t)
-            #tc += calcula_distancia(p1,p2)
+            p2 = self.bezier(t)
+            # length += distance(p1, p2)
             p1 = p2
-            t += dt 
-        p2 = self.bezier_calculation(1.0)
-        #tc += calcula_distancia(p1,p2)
+            t += dt
+        p2 = self.bezier(1.0)
+        # length += distance(p1, p2)
+        print(length)
 
-
-
-        pass
-
-    def bezier_calculation(self, t:float) -> Point:
+    def bezier(self, t:float) -> float:
         mt = 1-t
+        return (pow(mt,2) * self.p0) + ((2 * (mt * t)) * self.p1) + (pow(t, 2) * self.p2)
 
-        P = self.P[0] * mt  * mt + self.P[1] * 2 * mt * t + self.P[2] * t * t
-
-        return P
-
-
-
-
-"""
-void Bezier::calculaComprimentoDaCurva()
-{
-    double DeltaT = 1.0/50;
-    double t=DeltaT;
-    Ponto P1, P2;
-    ComprimentoTotalDaCurva = 0;
-    P1 = Calcula(0.0);
-    while(t<1.0)
-    {
-        P2 = Calcula(t);
-        ComprimentoTotalDaCurva += calculaDistancia(P1,P2);
-        P1 = P2;
-        t += DeltaT;
-    }
-    P2 = Calcula(1.0); // faz o fechamento da curva
-    ComprimentoTotalDaCurva += calculaDistancia(P1,P2);
-    cout << "ComprimentoTotalDaCurva: " << ComprimentoTotalDaCurva << endl;
-}
-"""
+    def distance(self, p1:Point, p2:Point) -> None:
+        # TODO
+        pass
